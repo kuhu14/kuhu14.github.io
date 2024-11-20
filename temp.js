20cc7af5cc1050f75f209fd1d67c5888b12bc829
Kuhu14 <kuhumgupta@gmail.com>
2024-11-20 17:48:35 +0530

    m

--- temp.js
+++ temp.js
@@ -23,16 +23,16 @@ db.Products.find();
 db.Products.insertMany(
     [
     {
-        "name":"Product 3",
-        "desc":"This is the description of Product 3",
-        "price":35,
-        "url":"https://picsum.photos/id/670/200/300"
+        "name":"Product 7",
+        "desc":"This is the description of Product 7",
+        "price":95,
+        "url":"https://picsum.photos/id/270/200/300"
     },
     {
-        "name":"Product 4",
-        "desc":"This is the description of Product 4",
-        "price":55,
-        "url":"https://picsum.photos/id/570/200/300"
+        "name":"Product 8",
+        "desc":"This is the description of Product 8",
+        "price":85,
+        "url":"https://picsum.photos/id/170/200/300"
     }
 ]
 );
