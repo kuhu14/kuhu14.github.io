ef341be0c6d6929fbe4ae453ce4bd7d1c4b516f0
amritha-00 <amrithavemuri@gmail.com>
2024-11-27 20:08:03 +0530

    ok

--- portfolio/src/Signup.js
+++ portfolio/src/Signup.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
-}
+    <div>
+        Signup
+        <p>
+            <input type="text" placeholder="Enter Name"></input>
+        </p>
+        <p>
+            <input type="text" placeholder="Enter Email"></input>
+        </p>
+        <p>
+            Date of Birth: <input type="datetime-local"></input>
+        </p>
+        <p>
+            Location: <input type="radio" name="Location"></input> Hyderabad <input type="radio" name="Location"></input> Delhi
+        </p>
+        <button>Sign Up</button>
+        <hr></hr>
+        <p>Already a member?</p>
+        <button> Login Here</button>
+    </div>
+  );
+};
